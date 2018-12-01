8af6dc1b396433d3fa1ed9a7aee63df51453842b
Gil Beyruth <firstpixel@gmail.com>
2018-12-01 17:16:47 -0200

    excluding list fixed, initial=1 to final=250 fixed

--- js/randomizer.js
+++ js/randomizer.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
     initNumber,lastNumber, finalTable, 
     excludeList = [], excludeNumbers;
 
-
     Array.prototype.contains = function(obj) {
         var i = this.length;
         while (i--) {
@@ -17,8 +16,6 @@ $(document).ready(function(){
 
     function createList(init,final) {
 
-
-
         for(var i=parseInt(init,10); i<=parseInt(final,10); i++) {
             if (excludeList.length) {
                 if(!excludeList.contains(i)){
@@ -28,12 +25,11 @@ $(document).ready(function(){
                 list.push(i);
             }
         }
-
     }
 
     function addRandom() {
+        $('#submitButton').attr("disabled", "disabled");
         number++;
-        
         if (number == 1 || (number-1) % 10 == 0) {
             out += "<tr>";
         }
@@ -47,7 +43,7 @@ $(document).ready(function(){
             fontSize: "130px"
           }, 600 );
 
-        out += '<td class="item">' + randomized + '</td>';
+        out += '<td class="item" style="width:10%">' + randomized + '</td>';
         if (number % 10 == 0) {
             out += "</tr>";
             finalTable = out;
@@ -56,26 +52,29 @@ $(document).ready(function(){
             finalTable = out;
             while(i % 10 != 0) {
                 i++;
-                finalTable+="<td>&nbsp;</td>";
+                finalTable+='<td style="width:10%">&nbsp;</td>';
             }
             finalTable+= "</tr>";
         }
         $('#result').html(finalTable);
         if (list.length == 0 && initNumber < lastNumber && lastNumber > 0 ) {
             $('#submitButton').attr("disabled", "disabled");
-        }
+        }else {
+            $('#submitButton').attr("disabled", null);
+        } 
     }
 
     $('#listContainer').hide();
-    $('#randomContainer').hide();
+    //$('#randomContainer').hide();
 
     // Handler for .ready() called.
     $("#initForm").submit(function(e) 
     {
         e.preventDefault();
-        initNumber = $('#initNumber').val();
-        lastNumber = $('#lastNumber').val();
-        excludeNumbers = $('#excludeNumbers').val();
+        initNumber = 1;//$('#initNumber').val();
+        lastNumber = 250;//$('#lastNumber').val();
+        //excludeNumbers = $('#excludeNumbers').val();
+        excludeNumbers = "11,12,13,14,15,16,17,18,19,25,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,139,140,141,142,143,144,145,146,147,148,149,150,155,156,161,197,211,212,213,219,220";
 
         if(initNumber < lastNumber) {
             if (!initialized) {
@@ -84,9 +83,9 @@ $(document).ready(function(){
                 }
                 createList(initNumber,lastNumber);
                 initialized = true;
-                $('#setupContainer').hide();
-                $('#excludeContainer').hide();
-                $('#listContainer').show(400);
+                //$('#setupContainer').hide();
+                //$('#excludeContainer').hide();
+                $('#listContainer').show();
                 $('#randomContainer').show();
                 addRandom();
             } else {
